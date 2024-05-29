7330f3c00d423cbbe2b9c3d27317fe0bf2bafd6e
Sandra Gao <Sandra Gao>
2024-05-29 14:34:52 -0400

    initial html generation

---
+++ py/final.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+print('Content-type: text/html\n')
+
+import cgitb
+cgitb.enable()
+import cgi
+
+def generate_page(body):
+	html_header = """<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	<meta charset='utf-8'>
+	<title>Home</title>
+	</head>
+	<body>\n"""
+	html_footer = """</body>
+	</html>"""
+
+	print(html_header, body, html_footer)
+generate_page('<p>hiihiihihihihih</p>')
