c86b5de08989bcfee315c0e068e700c0cb926ebb
Sandra Gao <Sandra Gao>
2024-05-28 20:56:33 -0400

    + html to w28

--- py/w28_rand.py
+++ py/w28_rand.py
@@ -2,4 +2,11 @@
 print('Content-type: text/html\n')
 
 from random import random
-print('generated: ', random())
+print ("""<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset='utf-8'>
+			<title>work 28</title>
+		</head>
+		<body> 
+			<h1> 'generated: '""", random(), """\n</h1></body></html>""")
