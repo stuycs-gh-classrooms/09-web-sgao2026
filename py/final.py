e7b7890143496f4d07b187356454eec9411d8591
Sandra Gao <Sandra Gao>
2024-05-31 14:34:20 -0400

    + years list

--- py/final.py
+++ py/final.py
@@ -5,13 +5,16 @@ import cgitb
 cgitb.enable()
 import cgi
 
+year_popular = {'year': [{'title': {'author': '', 'series': '', 'words': {}, 'chars': {}}}]}
+years = [00, 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
 def generate_page(body):
 	html_header = """<!DOCTYPE html>
 	<html lang="en">
 	<head>
 	<meta charset='utf-8'>
 	<title>Home</title>
-	<link href="../final/mystle.css" rel="stylesheet">
+	<link href="../final/mystyle.css" rel="stylesheet">
 	</head>
 	<body>\n"""
 	html_footer = """</body>
